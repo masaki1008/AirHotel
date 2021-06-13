61d50359ec7d0bedfecd4302fd29dee797c75c89
EC2 Default User <ec2-user@ip-172-31-7-196.ec2.internal>
2021-06-13 14:36:34 +0000

    パスワード変更更新

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
     def update_resource(resource, params)
       if params[:password].present? && params[:password_confirmation].present?
-        resource.update_attributes(params)
+        resource.update_with_password(params)
       else
         resource.update_without_password(params)
       end
