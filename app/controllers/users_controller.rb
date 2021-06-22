201062c201fc8733d6268ad2012842b704d69098
EC2 Default User <ec2-user@ip-172-31-7-196.ec2.internal>
2021-06-22 11:30:11 +0000

    完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,16 @@
 class UsersController < ApplicationController
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   protected
-  # deviseのpermitted_parameterを追加する
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:image] )
   end
-  
+
 end
--- db/migrate/20210620042309_add_prefecture_to_rooms.rb
+++
@@ -1,5 +0,0 @@
-class AddPrefectureToRooms < ActiveRecord::Migration[6.1]
-  def change
-    add_column :rooms, :prefecture, :string
-  end
-end
